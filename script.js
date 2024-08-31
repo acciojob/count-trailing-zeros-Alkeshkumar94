d080ba12c93384ae549e7b95a413e54b2b51e60d
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-08-31 18:08:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+	let fac=1;
+	while(n>0){
+		fac*=n;
+		n--;
+	}
+	let zero=0;
+   while((fac%10)===0){
+	   zero++;
+	   fac=Math.floor(fac/10);
+   }
+	return zero;
 }
 
 const input = prompt("Enter a number");
